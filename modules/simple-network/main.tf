9b911c18fc5561f54b71ae4e01e9b3e17c11c758
thomasmelison <thomasmelison@hotmail.com>
2024-05-02 16:28:58 -0300

    formatting network component

--- modules/simple-network/main.tf
+++ modules/simple-network/main.tf
@@ -1,13 +1,13 @@
 
 
 locals {
-  subnet_bits = ceil(log(var.maximum_subnet_number , 2))
-  host_bits = ceil(log(var.maximum_host_per_subnet + 2, 2))
+  subnet_bits = ceil(log(var.maximum_subnet_number, 2))
+  host_bits   = ceil(log(var.maximum_host_per_subnet + 2, 2))
 }
 
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/${32 - local.host_bits - local.subnet_bits}"
-  enable_dns_support = true 
+  cidr_block           = "10.0.0.0/${32 - local.host_bits - local.subnet_bits}"
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "${var.name_prefix}-vpc"
@@ -31,18 +31,18 @@ resource "aws_eip" "this" {
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.this.id
-  subnet_id     =  aws_subnet.public_subnet[var.availability_zones[0]].id
+  subnet_id     = aws_subnet.public_subnet[var.availability_zones[0]].id
 
   tags = {
     Name = "${var.name_prefix}-nat-gateway"
   }
 }
 
-resource "aws_route_table" "public" { 
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block = "0.0.0.0/0" 
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
 
@@ -51,27 +51,27 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table" "private" { 
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.this.id
   }
 
   tags = {
     Name = "${var.name_prefix}-private-route-table"
   }
-  
+
 }
 
 
 resource "aws_subnet" "public_subnet" {
   for_each = toset(var.availability_zones)
 
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, local.host_bits, index(var.availability_zones, each.value))
-  availability_zone = each.value
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, local.host_bits, index(var.availability_zones, each.value))
+  availability_zone       = each.value
   map_public_ip_on_launch = true
 
   tags = {
@@ -82,29 +82,29 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   for_each = toset(var.availability_zones)
 
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(
-    aws_vpc.this.cidr_block, 
-    local.host_bits, 
-    index(var.availability_zones, each.value) + ( var.maximum_subnet_number / 2 ) 
-    )
-  availability_zone = each.value
+  vpc_id = aws_vpc.this.id
+  cidr_block = cidrsubnet(
+    aws_vpc.this.cidr_block,
+    local.host_bits,
+    index(var.availability_zones, each.value) + (var.maximum_subnet_number / 2)
+  )
+  availability_zone       = each.value
   map_public_ip_on_launch = false
 
-    tags = {
+  tags = {
     Name = "${var.name_prefix}-${each.value}-private-subnet"
   }
 }
 
 resource "aws_route_table_association" "public" {
-	for_each = {for az, subnet in aws_subnet.public_subnet : az => subnet.id}
+  for_each = { for az, subnet in aws_subnet.public_subnet : az => subnet.id }
 
   subnet_id      = each.value
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-	for_each = {for az, subnet in aws_subnet.private_subnet : az => subnet.id}
+  for_each = { for az, subnet in aws_subnet.private_subnet : az => subnet.id }
 
   subnet_id      = each.value
   route_table_id = aws_route_table.private.id
--- modules/simple-network/variables.tf
+++ modules/simple-network/variables.tf
@@ -1,32 +1,32 @@
 variable "name_prefix" {
   description = "Prefix for the name of the provisioned resources."
-  type = string
+  type        = string
 }
 
-variable "availability_zones"{
-	description = "List of availability zones where the subnets will be created."
-  type = list(string)
+variable "availability_zones" {
+  description = "List of availability zones where the subnets will be created."
+  type        = list(string)
 }
 
 variable "maximum_subnet_number" {
   description = "The number of the maximum subnets that the infrastructure will have (if you plan to add more subnets at the future, if this number is changed the VPC might be recreated)."
-  type = number
-  default = 256
+  type        = number
+  default     = 256
 
   validation {
-    condition = var.maximum_subnet_number >= 2
+    condition     = var.maximum_subnet_number >= 2
     error_message = "Maximum number of subnets at the VPC it's too low. Minimum value is 2."
   }
 }
 
 variable "maximum_host_per_subnet" {
   description = "The maximum number of hosts that you expect to add in a single subnet."
-  type = number
-  default = 254
+  type        = number
+  default     = 254
 
   validation {
-    condition = var.maximum_host_per_subnet >= 4
+    condition     = var.maximum_host_per_subnet >= 4
     error_message = "Maximum number of hosts per subnet too low. Minimum value is 4."
   }
-  
+
 }
