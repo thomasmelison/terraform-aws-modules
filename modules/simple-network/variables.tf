9efe616033dae70370e005247cc0043117be981a
thomasmelison <thomasmelison@hotmail.com>
2024-05-02 13:04:08 -0300

    Fixing variable validation error

--- modules/simple-network/variables.tf
+++ modules/simple-network/variables.tf
@@ -12,26 +12,21 @@ variable "maximum_subnet_number" {
   description = "The number of the maximum subnets that the infrastructure will have (if you plan to add more subnets at the future, if this number is changed the VPC might be recreated)."
   type = number
   default = 256
+
+  validation {
+    condition = var.maximum_subnet_number >= 2
+    error_message = "Maximum number of subnets at the VPC it's too low. Minimum value is 2."
+  }
 }
 
 variable "maximum_host_per_subnet" {
   description = "The maximum number of hosts that you expect to add in a single subnet."
   type = number
   default = 254
-}
-
-variable "validate_network_settings" {
-  description = "Variable only used for validation of the other variables at this module."
-  type        = bool
-  default     = true
-  
 
   validation {
-    condition = (
-      local.subnet_bits >= 1 &&
-      local.host_bits >= 2 &&
-      (local.subnet_bits + local.host_bits) <= 24
-    )
-    error_message = "Network settings validation failed."
+    condition = var.maximum_host_per_subnet >= 4
+    error_message = "Maximum number of hosts per subnet too low. Minimum value is 4."
   }
+  
 }
