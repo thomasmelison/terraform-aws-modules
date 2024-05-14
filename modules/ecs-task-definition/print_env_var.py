9adcae331200f4617152b6a43772383387fd8582
thomasmelison <thomasmelison@hotmail.com>
2024-05-14 11:50:25 -0300

    deleting unecessary python script and dockerfile

--- modules/ecs-task-definition/print_env_var.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Define and print each environment variable individually
-print("VALOR DO OBJ KEY :", os.getenv("OBJECT_KEY", "Not Set"))
-print("VALOR DO BUCKET NAME :", os.getenv("BUCKET_NAME", "Not Set"))
-print("var1 valor :", os.getenv("var1", "Not Set"))
